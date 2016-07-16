4441573effe099364b8c6bf7fa5eb3f09fa9eb26
mlctrez <mlctrez@gmail.com>
2016-07-16 17:44:59 -0500

    add go format

---
+++ hooks/go_format.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import os
+
+import hookutils
+
+
+def format(files=None, from_git=True):
+    """
+    Executes go fmt on the provided files.
+    """
+
+    for file_in in files:
+
+        file_in = os.path.abspath(file_in)
+
+        before_hash = hookutils.hashfile(file_in)
+
+        hookutils.execute('gofmt %s' % file_in)
+
+        if from_git and hookutils.hashfile(file_in) != before_hash:
+            hookutils.execute('git add %s' % file_in)
+
+
+def run():
+    os.chdir(hookutils.git_toplevel())
+
+    go_files = filter(lambda x: x.endswith('.go'), hookutils.find_commits())
+
+    format(go_files, from_git=True)
+
+
+if __name__ == '__main__':
+    run()
